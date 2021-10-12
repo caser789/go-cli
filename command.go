d968d9c3a8c7658b31bbebeb62aa6e327c775dc8
Xue Jiao <jiao.xue@seamoney.com>
2021-10-12 18:19:38 +0800

    Allow for flags being passed in after arguments

--- command.go
+++ command.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 type Command struct {
@@ -24,7 +25,23 @@ func (c Command) Run(ctx *Context) {
 
 	set := flagSet(c.Name, c.Flags)
 	set.SetOutput(ioutil.Discard)
-	err := set.Parse(ctx.Args()[1:])
+
+	firstFlagIndex := -1
+	for index, arg := range ctx.Args() {
+		if strings.HasPrefix(arg, "-") {
+			firstFlagIndex = index
+			break
+		}
+	}
+
+	var err error
+	if firstFlagIndex > -1 {
+		args := ctx.Args()[1:firstFlagIndex]
+		flags := ctx.Args()[firstFlagIndex:]
+		err = set.Parse(append(flags, args...))
+	} else {
+		err = set.Parse(ctx.Args()[1:])
+	}
 
 	if err != nil {
 		fmt.Println("Incorrect Usage.")
